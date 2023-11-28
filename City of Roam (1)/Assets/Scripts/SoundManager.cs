d1c3409a232de6d3cf7a84f165b7c8e4170ec342
khanhhaphann <150897916+khanhhaphann@users.noreply.github.com>
2023-11-27 21:42:58 -0800

    Add files via upload

---
+++ City of Roam (1)/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoundManager : Monobehaviour
+{
+	[SerializeField]
+	private AudioSource m_SFXAudioSource;
+
+	[SerializeField]
+	private AudioSource m_MusicAudioSource;
+
+	private static SoundManager _instance;
+
+	void Awake()
+    {
+		if (_instance == null)
+        {
+			_instance = this;
+        }
+		else
+        {
+			Destroy(this);
+        }
+    }
+
+	public static void PlaySFX(AudioClip clip)
+    {
+		_instance.m_SFXAudioSource.PlayOneShot(clip);
+    }
+
+	public static void PlayMusic(AudioClip clip)
+    {
+		_instance.m_MusicAudioSource.Stop();
+		_instance.m_MusicAudioSource.clip = clip;
+		_instance.m_MusicAudioSource.Play();
+    }
+	
+}
