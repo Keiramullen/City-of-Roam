ff828d9a398e6d6e9277c2393369d9a704a874c7
khanhhaphann <khphan3@uci.edu>
2023-11-30 16:10:34 -0800

    SoundManager

--- City of Roam (1)/Assets/Scripts/SoundManager.cs
+++ City of Roam (1)/Assets/Scripts/SoundManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class SoundManager : Monobehaviour
+public class SoundManager : MonoBehaviour
 {
 	[SerializeField]
 	private AudioSource m_SFXAudioSource;
